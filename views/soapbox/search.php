a88cd56f4592e9f22c134ee6203ee831d74ec93f
Dave Widmer <dave@davewidmer.net>
2011-12-12 10:11:53 -0500

    Fixing the updated search route.

--- views/soapbox/search.php
+++ views/soapbox/search.php
@@ -1,5 +1,5 @@
 <div id="search">
-	<form action="<?php echo Route::url('soapbox/search'); ?>" method="" id="search-form">
+	<form action="<?php echo Route::url('soapbox/action', array('action' => "search")); ?>" method="" id="search-form">
 		<label for="search">Search</label>
 		<input type="search" name="query" value="<?php echo Request::current()->query('query'); ?>" id="search" />
 
