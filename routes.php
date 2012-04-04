59c161aaf2fb168d5acd488709e993bd4483b99d
Dave Widmer <dave@davewidmer.net>
2012-04-04 10:09:26 -0400

    Moving the routes into its own file.

---
+++ routes.php
@@ -0,0 +1,56 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Admin
+Route::set('soapbox/admin', "admin(/<action>(/<id>))", array(
+	'action' => "(|add|edit|delete)",
+	'id' => "\d+"
+))->defaults(array(
+	'directory' => "admin",
+	'controller' => "soapbox",
+	'action' => "index",
+	'id' => null
+));
+
+// Admin categories
+Route::set('soapbox/admin/category', "admin/category(/<action>(/<id>))", array(
+	'action' => "(|add|edit|delete)",
+	'id' => "\d+"
+))->defaults(array(
+	'directory' => "admin",
+	'controller' => "soapbox_category",
+	'action' => "index",
+	'id' => null
+));
+
+// Login
+Route::set('soapbox/login', "<action>", array('action' => "(login|logout)"))
+->defaults(array(
+	'controller' => "soapbox",
+));
+
+// Single Post
+Route::set('soapbox/post', "<year>/<month>/<slug>", array(
+	'year' => "\d{4}",
+	'month' => "\d{2}",
+	'slug' => ".*"
+))->defaults(array(
+	'controller' => "soapbox",
+	'action'	=> "post",
+	'year'	=> null,
+	'month'	=> null,
+	'slug'	=> null,
+));
+
+// Category
+Route::set('soapbox/category', "category/<category>", array('category' => ".*"))
+->defaults(array(
+	'controller' => "soapbox",
+	'action'	=> "category",
+));
+
+// One word routes
+Route::set('soapbox/action', "{$section}/<action>", array('action' => "(|search|feed|404)"))
+->defaults(array(
+	'controller' => "soapbox",
+	'action' => "index",
+));
