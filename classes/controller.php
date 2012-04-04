008f5507e3670b88dd9f9c50204b9d34f8a482a9
Dave Widmer <dave@davewidmer.net>
2012-04-04 10:32:12 -0400

    Creating a base controller.

---
+++ classes/controller.php
@@ -0,0 +1,80 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * The Controller handles loading the correct layout as well as rendering the
+ * output correctly.
+ *
+ * @package    Soapbox
+ * @author     Dave Widmer <dave@davewidmer.net>
+ * @copyright  2011-2012 © Dave Widmer
+ */
+class Controller extends Kohana_Controller
+{
+	/**
+	 * @var \Owl\Layout  The layout view file
+	 */
+	protected $layout;
+
+	/**
+	 * @var mixed The content to add to the layout
+	 */
+	protected $content;
+
+	/**
+	 * @var User The currently logged in user
+	 */
+	protected $user = null;
+
+	/**
+	 * @var boolean  Is the request from a mobile device?
+	 */
+	protected $is_mobile = false;
+
+	/**
+	 * Gets the logged in user if they are available
+	 */
+	public function before()
+	{
+		// See which request to load
+		$layout = "View_Layout_Browser";
+
+		$detect = new Mobile_Detect;
+
+		if ($detect->isMobile())
+		{
+			$layout = "View_Layout_Mobile";
+			$this->is_mobile = true;
+		}
+
+		// Create the layout and setup needed variables
+		$this->layout = new $layout;
+		$this->layout->set_engine(new Mustache);
+		$this->layout->set_template_path(APPPATH."views");
+
+		return parent::before();
+	}
+
+	/**
+	 * Renders the content either in the layout or json encoded, depending on
+	 * the request. 
+	 */
+	public function after()
+	{
+		if ($this->request->is_ajax())
+		{
+			$type = "application/json";
+			$content = json_encode($this->content);
+		}
+		else
+		{
+			$this->layout->set_content($this->content);
+
+			$type = "text/html";
+			$content = $this->layout->render();
+		}
+
+		// Send the proper response
+		$this->response->headers("Content-Type", $type)->body($content);
+	}
+
+}
