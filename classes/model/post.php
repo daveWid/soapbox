b1dbc024e3c4e1e6a7c2e9515767ccaeec6bf152
Dave Widmer <dave@davewidmer.net>
2012-04-04 12:10:59 -0400

    Adding the Post entity and model classes.

--- classes/model/post.php
+++ classes/model/post.php
@@ -1,3 +1,45 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Post extends Soapbox_Model_Post{}
+/**
+ * The model that gets data from the `posts` table.
+ *
+ * @package    Soapbox
+ * @author     Dave Widmer <dave@davewidmer.net>
+ * @copyright  2011-2012 © Dave Widmer
+ */
+class Model_Post extends \Cactus\Model
+{
+	/**
+	 * Setup for the model
+	 */
+	public function __construct()
+	{
+		parent::__construct(array(
+			'table' => "post",
+			'primary_key' => "post_id",
+			'columns' => array(
+				'post_id' => \Cactus\Field::INT,
+				'title' => \Cactus\Field::VARCHAR,
+				'slug' => \Cactus\Field::VARCHAR,
+				'content' => \Cactus\Field::VARCHAR,
+				'posted_date' => \Cactus\Field::DATETIME
+			),
+			'object_class' => "Soapbox_Post",
+		));
+	}
+
+	/**
+	 * Get the latest posts.
+	 *
+	 * @param  int $num  The number of posts to try and find
+	 * @return \Cactus\Collection
+	 */
+	public function latest($num = 10)
+	{
+		return $this->find(array(
+			'order_by' => 'posted_date',
+			'limit' => $num
+		));
+	}
+
+}
---
+++ classes/soapbox/post.php
@@ -0,0 +1,42 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * A class representation of a row in the posts table.
+ *
+ * @package    Soapbox
+ * @author     Dave Widmer <dave@davewidmer.net>
+ * @copyright  2011-2012 © Dave Widmer
+ */
+class Soapbox_Post extends \Cactus\Entity
+{
+	/**
+	 * The link to the full post.
+	 *
+	 * @return string 
+	 */
+	public function link()
+	{
+		return URL::site($this->date("Y/m/").$this->slug);
+	}
+
+	/**
+	 * A human readable date of when the post was posted.
+	 *
+	 * @param  string $format  The date format
+	 * @return string
+	 */
+	public function date($format = "F jS, Y")
+	{
+		return $this->posted_date->format($format);
+	}
+
+	/**
+	 * Are there categories with this post? 
+	 *
+	 * @return boolean
+	 */
+	public function has_categories()
+	{
+		return count($this->categories) > 0;
+	}
+}
