777872db46bc673222a40194a8230b7665e938ef
Dave Widmer <dave@davewidmer.net>
2012-04-10 08:59:32 -0400

    Tightening up the routing.

--- routes.php
+++ routes.php
@@ -22,25 +22,6 @@ Route::set('soapbox/admin/category', "admin/category(/<action>(/<id>))", array(
 	'id' => null
 ));
 
-// Login
-Route::set('soapbox/login', "<action>", array('action' => "(login|logout)"))
-->defaults(array(
-	'controller' => "soapbox",
-));
-
-// Single Post
-Route::set('soapbox/post', "<year>/<month>/<slug>", array(
-	'year' => "\d{4}",
-	'month' => "\d{2}",
-	'slug' => ".*"
-))->defaults(array(
-	'controller' => "soapbox",
-	'action'	=> "post",
-	'year'	=> null,
-	'month'	=> null,
-	'slug'	=> null,
-));
-
 // Category
 Route::set('soapbox/category', "category/<category>", array('category' => ".*"))
 ->defaults(array(
@@ -49,8 +30,15 @@ Route::set('soapbox/category', "category/<category>", array('category' => ".*"))
 ));
 
 // One word routes
-Route::set('soapbox', "<action>", array('action' => "(|search|feed|404)"))
+Route::set('soapbox', "<action>", array('action' => "(|archive|search|feed|404|login|logout)"))
 ->defaults(array(
 	'controller' => "soapbox",
 	'action' => "index",
+));
+
+// Single Post
+Route::set('soapbox/post', "<slug>", array('slug' => ".*")
+)->defaults(array(
+	'controller' => "soapbox",
+	'action'	=> "post",
 ));
