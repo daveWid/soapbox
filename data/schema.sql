907486ed39df1f37a0ae5c0c40d6bfa47beace51
Dave Widmer <dave@davewidmer.net>
2012-04-10 17:02:43 -0400

    Updating the database schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,31 +1,86 @@
-CREATE TABLE `category` (
-  `category_id` int(10) unsigned NOT NULL auto_increment,
+-- phpMyAdmin SQL Dump
+-- version 3.3.2deb1ubuntu1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 10, 2012 at 05:01 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.2-1ubuntu4.14
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `soapbox`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `slug` varchar(255) NOT NULL,
   `display` varchar(255) NOT NULL,
-  PRIMARY KEY  (`category_id`)
+  PRIMARY KEY (`category_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
 
 INSERT INTO `category` (`category_id`, `slug`, `display`) VALUES
 (1, 'uncategorized', 'Uncategorized');
 
-CREATE TABLE `post` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
   `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `posted_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`post_id`)
+  `source` text NOT NULL,
+  `html` text NOT NULL,
+  `excerpt` varchar(255) NOT NULL,
+  `posted_date` date NOT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`post_id`),
+  FULLTEXT KEY `title` (`title`),
+  FULLTEXT KEY `title_2` (`title`,`source`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-INSERT INTO `post` (`post_id`, `title`, `slug`, `content`, `posted_date`) VALUES
-(1, 'Hello World', 'hello-world', '<p>Here is a sample post that you should delete or edit to your liking!</p>', '2011-04-21');
+--
+-- Dumping data for table `post`
+--
 
-CREATE TABLE `post_category` (
+INSERT INTO `post` (`post_id`, `title`, `slug`, `source`, `html`, `excerpt`, `posted_date`, `last_modified`) VALUES
+(1, 'Hello World', '2011/04/hello-world', 'Here is a sample post that you should delete or edit to your liking!', '<p>Here is a sample post that you should delete or edit to your liking!</p>', '<p>Here is a sample post that you should delete or edit to your liking!</p>', '2011-04-10', '2011-04-10 12:00:00');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_category`
+--
+
+CREATE TABLE IF NOT EXISTS `post_category` (
   `post_id` int(10) unsigned NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
   KEY `post_id` (`post_id`),
   KEY `category_id` (`category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `post_category`
+--
 
 INSERT INTO `post_category` (`post_id`, `category_id`) VALUES
 (1, 1);
